cd157ff4040252ee78edd0efbc7634157992db6d
Hoowah <spectralink123@gmail.com>
2017-10-02 17:55:13 -0600

    Reformat common.h

--- mobile/src/main/cpp/jni-include/common.h
+++ mobile/src/main/cpp/jni-include/common.h
@@ -1,6 +1,6 @@
-#define PJ_CONFIG_ANDROID   1
-#define PJ_IS_LITTLE_ENDIAN 1
-#define PJ_IS_BIG_ENDIAN 0
+#define PJ_CONFIG_ANDROID       1
+#define PJ_IS_LITTLE_ENDIAN     1
+#define PJ_IS_BIG_ENDIAN        0
 
 #include <pjsua-lib/pjsua.h>
 #include <jni.h>
